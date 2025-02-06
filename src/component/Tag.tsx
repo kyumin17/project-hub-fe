b4e332cc4aa9bec231ef60e09b812d8fa2fb09b6
kyumin <kyumin2805@gmail.com>
2025-02-06 20:04:33 +0900

    [feat] add image in tag

--- src/component/Tag.tsx
+++ src/component/Tag.tsx
@@ -1,16 +1,25 @@
 import styled from 'styled-components';
+import data from '../data/tag_img_path.json';
 
 const TagWrapper = styled.div`
-  background-color: #E9F6FF;
-  color: #3E7BFF;
-  display: inline;
-  padding: 1.5px 5px;
+  display: flex;
+  padding: 3px 8px 3px 4px;
   border-radius: 3px;
+  align-items: center;
+  gap: 8px;
+  border: 1px solid #bebebe;
+`;
+
+const TagIcon = styled.img`
+  height: 18px;
 `;
 
 export default function Tag({ label }) {
+  const path = `/img/tag/${data[label].path}`;
+
   return (
     <TagWrapper>
+      <TagIcon src={path} alt='' />
       {label}
     </TagWrapper>
   );
--- src/component/ToggleTag.tsx
+++ src/component/ToggleTag.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { useState } from 'react';
+import data from '../data/tag_img_path.json';
 
 interface TagWrapperProps {
   isactive: boolean;
@@ -8,14 +9,22 @@ interface TagWrapperProps {
 const TagWrapper = styled.div<TagWrapperProps>`
   background-color: ${(props) => (props.isactive ? '#E9F6FF' : '#F5F5F5')};
   color: ${(props) => (props.isactive ? '#3E7BFF' : '#8D8D8D')};
-  display: inline;
   padding: 1.5px 5px;
   border-radius: 3px;
   cursor: pointer;
+  display: flex;
+  padding: 3px 8px 3px 4px;
+  align-items: center;
+  gap: 8px;
+`;
+
+const TagIcon = styled.img`
+  height: 18px;
 `;
 
 export default function ToggleTag({ label, techList, setTechList }) {
   const [isActive, setIsActive] = useState(false);
+  const path = `/img/tag/${data[label].path}`;
 
   function activateTag() {
     setIsActive(!isActive);
@@ -24,6 +33,7 @@ export default function ToggleTag({ label, techList, setTechList }) {
 
   return (
     <TagWrapper isactive={isActive} onClick={activateTag}>
+      <TagIcon src={path} alt='' />
       {label}
     </TagWrapper>
   );
